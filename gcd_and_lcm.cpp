6341db6d32fcbabfa810e0dd7f7987cf537c1c97
Pavel Dolgov <pdolgov99@gmail.com>
2015-05-24 00:04:27 +0300

    Use gcd and lcm abbreviations instead of nod, nok
    gcd is "greatest common divisor" and lcm is "least common
    multiple".

---
+++ gcd_and_lcm.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int gcd(int a, int b) {
+    while (b) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int lcm(int a, int b) {
+    return (a * b) / gcd(a, b);
+}
+
+void lcmAndGcd(int* arr, int size) {
+    int lcm1 = lcm(arr[0], arr[1]);
+    int gcd1 = gcd(arr[0], arr[1]);
+    for (int i = 2; i < size; i++) {
+        lcm1 = lcm(lcm1, arr[i]);
+        gcd1 = gcd(gcd1, arr[i]);
+    }
+    std::cout << lcm1 << std::endl;
+    std::cout << gcd1 << std::endl;
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    int* arr = new int[n];
+    for (int i = 0; i < n; i++) {
+        std::cin >> arr[i];
+    }
+    lcmAndGcd(arr, n);
+    delete[] arr;
+    return 0;
+}
