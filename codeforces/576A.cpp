0ef711b50cca6a82780b63916086a45591eafa24
Pavel Dolgov <pdolgov99@gmail.com>
2015-09-16 22:40:42 +0300

    Add the second codeforces problem

---
+++ codeforces/576A.cpp
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2015 Pavel Dolgov
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+/* http://codeforces.com/contest/527/submission/13054549 */
+
+#include <iostream>
+#include <vector>
+
+int main() {
+    int n;
+    std::cin >> n;
+    /* Sieve of Eratosthenes */
+    std::vector<char> prime(n+1, true);
+    prime[0] = prime[1] = false;
+    for (int i = 2; i <= n; i++) {
+        if (1ll * i * i <= n) {
+            for (int x = i * i; x <= n; x += i) {
+                prime[x] = false;
+            }
+        }
+    }
+    typedef std::vector<int> Ints;
+    Ints result;
+    for (int i = 0; i <= n; i++) {
+        if (prime[i]) {
+            int s = 1;
+            while (s <= n / i) {
+                s *= i;
+                result.push_back(s);
+            }
+        }
+    }
+    std::cout << result.size() << std::endl;
+    for (Ints::const_iterator i = result.begin(); i != result.end(); i++) {
+        std::cout << *i << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
