195a034cbb76d2caf0c3259c818d99bb65fdcba1
Pavel Dolgov <pdolgov99@gmail.com>
2015-09-25 17:26:43 +0300

    Codeforces: 500A

---
+++ codeforces/500A.cpp
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2015 Pavel Dolgov
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+/* http://codeforces.com/contest/500/submission/13220211 */
+
+#include <iostream>
+
+int main() {
+    int n, t;
+    std::cin >> n >> t;
+    bool possibility = false;
+    int curr = 1;
+    for (int i = 1; i < n; i++) {
+        int number;
+        std::cin >> number;
+        if (i == curr) {
+            curr += number;
+        }
+        if (curr == t) {
+            possibility = true;
+        }
+    }
+    std::cout << (possibility ? "YES" : "NO") << std::endl;
+    return 0;
+}
