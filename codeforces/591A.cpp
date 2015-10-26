836b5db3f0430c7f988f886f028869159c24d5a9
Pavel Dolgov <pdolgov99@gmail.com>
2015-10-26 22:34:13 +0300

    Codeforces: 591A

---
+++ codeforces/591A.cpp
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2015 Pavel Dolgov
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+/* http://codeforces.com/contest/591/submission/13878629 */
+
+#include <bits/stdc++.h>
+
+int main() {
+    int l, p, q;
+    std::cin >> l >> p >> q;
+    double time = (double) l / (double) (p + q);
+    std::cout << time * p << std::endl;
+    return 0;
+}
