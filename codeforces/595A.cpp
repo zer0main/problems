2396f672672b099932487e32ac3bf88316b08951
Pavel Dolgov <pdolgov99@gmail.com>
2015-11-16 22:08:41 +0300

    Codeforces: 595A

---
+++ codeforces/595A.cpp
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2015 Pavel Dolgov
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+/* http://codeforces.com/contest/556/submission/14302773 */
+
+#include <bits/stdc++.h>
+
+int main() {
+    int n, m;
+    std::cin >> n >> m;
+    int ans = 0;
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = 0; j < m; j++) {
+            int window1, window2;
+            std::cin >> window1 >> window2;
+            if (window1 || window2) {
+                ans++;
+            }
+
+        }
+    }
+    std::cout << ans << std::endl;
+    return 0;
+}
