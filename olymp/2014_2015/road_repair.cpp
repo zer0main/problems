49f89e10e746438623cf9d3c6c3aedd5c1fd1ebb
Pavel Dolgov <pdolgov99@gmail.com>
2015-12-08 21:52:18 +0300

    Olympiad: road_repair problem

---
+++ olymp/2014_2015/road_repair.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+#define MIN -100500
+
+int main() {
+    int L, N;
+    std::cin >> L >> N;
+    int last = MIN;
+    int ans = 0;
+    for (int i = 0; i < N; i++) {
+        int state;
+        std::cin >> state;
+        if (state == 1) {
+            if ((i - last + 1) > L) {
+                last = i;
+                ans++;
+            }
+        }
+    }
+    std::cout << ans << std::endl;
+    return 0;
+}
