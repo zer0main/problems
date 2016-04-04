eea127d0c0e338ec8a414918b022cde6a3a5b21c
Pavel Dolgov <pdolgov99@gmail.com>
2016-04-05 00:00:09 +0300

    Knapsack problem with one parameter

---
+++ mshp/algo/dynamic_programming/gold.cpp
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2015-2016 Pavel Dolgov
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#include <bits/stdc++.h>
+
+#define SIZE 105
+#define MAX_MASS 10500
+
+int gold[SIZE];
+
+int D[MAX_MASS];
+
+int main() {
+    int N, M;
+    std::cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        std::cin >> gold[i];
+    }
+    D[0] = 1;
+    for (int i = 1; i <= M; i++) {
+        D[i] = 0;
+    }
+    for (int i = 0; i < N; i++) {
+        for (int j = M - gold[i]; j >= 0; j--) {
+            D[j + gold[i]] |= D[j];
+        }
+    }
+    int max = 0;
+    for (int i = 0; i <= M; i++) {
+        if (D[i] == 1) {
+            max = i;
+        }
+    }
+    std::cout << max << std::endl;
+    return 0;
+}
