23663bd32d3d769a092da24345c0e9b36e240805
Pavel Dolgov <pdolgov99@gmail.com>
2016-05-07 22:43:36 +0300

    Add generator problem

---
+++ mshp/algo/brute_force/generator.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2015-2016 Pavel Dolgov
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#include <bits/stdc++.h>
+
+int N, K;
+
+void translate(int n) {
+    std::vector<int> res(N, 1);
+    int index = N - 1;
+    while (n) {
+        res[index--] = (n % K) + 1;
+        n /= K;
+    }
+    for (int i = 0; i < res.size(); i++) {
+        std::cout << res[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    std::cin >> N >> K;
+    int max = static_cast<int>(std::pow(K, N));
+    for (int i = 0; i < max; i++) {
+        translate(i);
+    }
+    return 0;
+}
