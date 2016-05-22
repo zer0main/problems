29671efbdeabb192412cc6e78229c2b283a9c44b
Pavel Dolgov <pdolgov99@gmail.com>
2016-05-23 01:48:49 +0300

    Geometric primitives: polar angle

---
+++ mshp/algo/geo/polar_angle.cpp
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2015-2016 Pavel Dolgov
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#include <bits/stdc++.h>
+
+#define PI 3.1415926
+
+int main() {
+    int x, y;
+    std::cin >> x >> y;
+    double ans = std::atan2(y, x);
+    ans = ((ans < 0) ? (2 * PI + ans) : ans);
+    std::cout << std::setprecision(6) << std::fixed;
+    std::cout << ans << std::endl;
+    return 0;
+}
