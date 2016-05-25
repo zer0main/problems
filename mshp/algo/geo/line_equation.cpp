a2e54c8d1374970d1e8848ce936416cfe6c941b0
Pavel Dolgov <pdolgov99@gmail.com>
2016-05-25 13:33:09 +0300

    Geometric primitives: line equation

---
+++ mshp/algo/geo/line_equation.cpp
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2015-2016 Pavel Dolgov
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#include <bits/stdc++.h>
+
+int main() {
+    int x0, y0, x1, y1;
+    std::cin >> x0 >> y0 >> x1 >> y1;
+    int A = y1 - y0;
+    int B = x0 - x1;
+    int C = -A * x0 - B * y0;
+    std::cout << A << " " << B << " " << C << std::endl;
+    return 0;
+}
