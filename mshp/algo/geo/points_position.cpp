d94e7c3e26e091ec710eb9c56f49f2db90ee2654
Pavel Dolgov <pdolgov99@gmail.com>
2016-05-27 23:20:03 +0300

    Geometric primitives: points position

---
+++ mshp/algo/geo/points_position.cpp
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2015-2016 Pavel Dolgov
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#include <bits/stdc++.h>
+
+int main() {
+    // points coordinates
+    int x0, y0, x1, y1;
+    // line
+    int A, B, C;
+    std::cin >> x0 >> y0 >> x1 >> y1 >> A >> B >> C;
+    bool p0_value = std::signbit((double) (A * x0 + B * y0 + C));
+    bool p1_value = std::signbit((double) (A * x1 + B * y1 + C));
+    std::cout << ((p0_value == p1_value) ? "YES" : "NO") << std::endl;
+    return 0;
+}
