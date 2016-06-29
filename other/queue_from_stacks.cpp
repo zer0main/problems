5725b44bfd084c1b7271702eb868be2642fbed7c
Pavel Dolgov <pdolgov99@gmail.com>
2016-06-29 22:14:10 +0300

    queue_from_stacks implementation

--- other/queue_from_stacks.cpp
+++ other/queue_from_stacks.cpp
@@ -8,7 +8,36 @@
 
 // The problem is to make queue from two stacks.
 
-int main() {
+std::stack<int> input, output;
 
+void move() {
+    while (!input.empty()) {
+        int element = input.top();
+        input.pop();
+        output.push(element);
+    }
+}
+
+enum Command {
+    PUSH,
+    POP
+};
+
+int main() {
+    while (true) {
+        int command, arg;
+        std::cin >> command;
+        if (command == PUSH) {
+            std::cin >> arg;
+            input.push(arg);
+        } else if (command == POP) {
+            if (output.empty()) {
+                move();
+            }
+            output.pop();
+        } else {
+            std::cerr << "Invalid command." << std::endl;
+        }
+    }
     return 0;
 }
