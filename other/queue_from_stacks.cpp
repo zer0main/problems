903cecdcf64bf9d529874bbe66825117ef733faf
Pavel Dolgov <pdolgov99@gmail.com>
2016-06-29 21:53:15 +0300

    Queue from two stacks: skeleton and description

---
+++ other/queue_from_stacks.cpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2015-2016 Pavel Dolgov
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#include <bits/stdc++.h>
+
+// The problem is to make queue from two stacks.
+
+int main() {
+
+    return 0;
+}
