1db53b1f1df74a88d92113c6af2d6b907ac1f7a1
Pavel Dolgov <pdolgov99@gmail.com>
2016-06-29 22:28:24 +0300

    Add description of frequent_element problem

---
+++ other/frequent_element.cpp
@@ -0,0 +1,6 @@
+/**
+  The majority element is the element that occurs more
+  than half of the size of the array.
+  How to find the majority element in an array in O(n)?
+  There is a guarantee that it exists.
+*/
