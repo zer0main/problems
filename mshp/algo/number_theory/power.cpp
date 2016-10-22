67be30952b26a9c47a8c83c170c1daad0fb3de81
Pavel Dolgov <pdolgov99@gmail.com>
2016-10-22 23:36:43 +0300

    Add power problem
    Calculate a ** b with precision 1e6.

---
+++ mshp/algo/number_theory/power.cpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2015-2016 Pavel Dolgov
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#include <bits/stdc++.h>
+
+typedef long long int lint;
+
+lint PRECISION = (lint) 1e6;
+
+lint myPower(lint a, lint b) {
+    lint ans = 1;
+    for (lint pw = 0; pw < b; pw++) {
+        ans = ((a % PRECISION) * (ans % PRECISION)) % PRECISION;
+    }
+    return ans;
+}
+
+int main() {
+    lint a, b;
+    std::cin >> a >> b;
+    std::cout << myPower(a, b) << std::endl;
+    return 0;
+}
