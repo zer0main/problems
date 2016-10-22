877bbbb0e06fe3ed17ed7147c839b6f868f01a4d
Pavel Dolgov <pdolgov99@gmail.com>
2016-10-22 23:34:38 +0300

    Add primes problem
    Compute nth prime number.

---
+++ mshp/algo/number_theory/primes.cpp
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2015-2016 Pavel Dolgov
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#include <bits/stdc++.h>
+
+const int N = 16000000;
+int lp[N + 1];
+int pr[N + 1];
+int pr_size = 0;
+
+int main() {
+    int number;
+    std::cin >> number;
+    for (int i = 2; i <= N; i++) {
+        if (lp[i] == 0) {
+            lp[i] = i;
+            pr[pr_size++] = i;
+        }
+        for (int j = 0; j < pr_size && pr[j] <= lp[i] && i * pr[j] <= N; ++j)
+            lp[i * pr[j]] = pr[j];
+    }
+    std::cout << pr[number - 1] << std::endl;
+    return 0;
+}
