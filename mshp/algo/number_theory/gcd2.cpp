0c14a16410966c598894986cb4ee507d1e3e69fe
Pavel Dolgov <pdolgov99@gmail.com>
2016-10-22 23:32:38 +0300

    Add simple gcd-based problem

---
+++ mshp/algo/number_theory/gcd2.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2015-2016 Pavel Dolgov
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#include <bits/stdc++.h>
+
+typedef long long int lint;
+
+/* gcd means "greatest common divisor" */
+lint gcd(lint a, lint b) {
+    while (b) {
+        lint temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    lint n, var;
+    std::cin >> n >> var;
+    lint ans = gcd(var, var);
+    for (lint i = 1; i < n; i++) {
+        std::cin >> var;
+        ans = gcd(ans, var);
+    }
+    std::cout << ans << std::endl;
+    return 0;
+}
