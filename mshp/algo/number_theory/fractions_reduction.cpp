a82c1dbf40c69987e8a4a7c456ba040a473c38cc
Pavel Dolgov <pdolgov99@gmail.com>
2016-10-22 23:33:12 +0300

    Add fractions reduction problem

---
+++ mshp/algo/number_theory/fractions_reduction.cpp
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2015-2016 Pavel Dolgov
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#include <bits/stdc++.h>
+
+typedef long long int lint;
+
+/* gcd means "greatest common divisor" */
+lint gcd(lint a, lint b) {
+    while (b) {
+        lint temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    lint a, b;
+    std::cin >> a >> b;
+    lint gcd_val = gcd(a, b);
+    lint nominator = a / gcd_val;
+    lint denominator = b / gcd_val;
+    // stupid testing system doesn't understand that a / -b = -a / b
+    if (denominator < 0) {
+        nominator = -std::abs(nominator);
+        denominator = std::abs(denominator);
+    }
+    std::cout << nominator << " " << denominator << std::endl;
+    return 0;
+}
