19d5bea075bfe6f8ad49a9b230868f71dd33d2f2
Pavel Dolgov <pdolgov99@gmail.com>
2018-10-01 02:23:45 +0100

    codeforces: 518D

---
+++ codeforces/518D.cpp
@@ -0,0 +1,45 @@
+/* http://codeforces.com/contest/518/submission/43627559 */
+
+#include <bits/stdc++.h>
+
+#define MAX_N 2000
+#define MAX_T 2000
+
+double probabilities[MAX_T][MAX_N];
+
+int main() {
+    int n, t;
+    double p, ans = 0;
+    std::cin >> n >> p >> t;
+    if (n >= t) {
+        // Mean of binomial distribution.
+        ans = t * p;
+    } else {
+        // Base.
+        probabilities[0][0] = 1 - p;
+        probabilities[0][1] = p;
+        for (int i = 1; i < t; i++) {
+            int max_folks = i + 1;
+            if (max_folks > n) {
+                max_folks = n;
+            }
+            for (int j = 0; j <= max_folks; j++) {
+                double entered = probabilities[i - 1][j - 1] * p;
+                double stayed = probabilities[i - 1][j] * (1 - p);
+                if (j == n) {
+                    stayed = probabilities[i - 1][j];
+                }
+                if (j == 0) {
+                    entered = 0;
+                }
+                probabilities[i][j] = entered + stayed;
+            }
+        }
+        for (int i = 0; i <= n; i++) {
+            ans += probabilities[t - 1][i] * i;
+        }
+    }
+    std::cout << std::setprecision(7) << std::fixed;
+    std::cout << ans << std::endl;
+    return 0;
+}
